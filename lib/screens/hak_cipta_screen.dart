50b7eaaa6ddd58e11c18b7d7a5637475c307de4f
hafidzniioman <hafidzniioman@gmail.com>
2019-06-27 22:35:07 +0700

    hak cipta update

--- lib/screens/hak_cipta_screen.dart
+++ lib/screens/hak_cipta_screen.dart
@@ -1,40 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:master_repo/widget/container_icon.dart';
 
-class HakCiptaScreen extends StatelessWidget{
+class HakCiptaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("HAK CIPTA SCREEN"),
-      ),
-      body: Container(
-        color: Colors.teal,
-        margin: EdgeInsets.all(5.0),
-        child: GridView.count(
-          crossAxisCount: 3,
-          children: <Widget>[
-            ContainerIcon(imageIndex: 1),
-            ContainerIcon(imageIndex: 2),
-            ContainerIcon(imageIndex: 3),
-            ContainerIcon(imageIndex: 4),
-            ContainerIcon(imageIndex: 5),
-            ContainerIcon(imageIndex: 6),
-            ContainerIcon(imageIndex: 7),
-            ContainerIcon(imageIndex: 8),
-            ContainerIcon(imageIndex: 9),
-            ContainerIcon(imageIndex: 10),
-            ContainerIcon(imageIndex: 11),
-            ContainerIcon(imageIndex: 12),
-            ContainerIcon(imageIndex: 13),
-            ContainerIcon(imageIndex: 14),
-            ContainerIcon(imageIndex: 15),
-            ContainerIcon(imageIndex: 16),
-            ContainerIcon(imageIndex: 17),
-            ContainerIcon(imageIndex: 18),
-          ],
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          title: const Text('Hak Cipta'),
         ),
-      ),
+        SliverList(delegate: SliverChildListDelegate(buildTextViews(50))),
+      ],
     );
+  }
+
+  List buildTextViews(int count) {
+    List<Widget> strings = List();
+    for (int i = 0; i < count; i++) {
+      strings.add(new Padding(
+          padding: new EdgeInsets.all(16.0),
+          child: new Text("Item number " + i.toString(),
+              style: new TextStyle(fontSize: 20.0))));
+    }
+    return strings;
   }
 }
