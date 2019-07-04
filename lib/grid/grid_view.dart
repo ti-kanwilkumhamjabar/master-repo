65279e32b207ea503aa6ff252358387cd8b3e577
ti-kanwilkumhamjabar <TI.Kumhamjabar@gmail.com>
2019-07-04 13:01:39 +0700

    added grid_view.dart

--- lib/grid/grid_view.dart
+++ lib/grid/grid_view.dart
@@ -10,19 +10,44 @@ class _BodyGridState extends State<BodyGrid> {
 
   var list_grid = [
     {
+      "title":"yankum",
+      "picture":"images/icon_new/1.png",
+      "description":"Yankum"
+    },
+    {
+      "title":"uud",
+      "picture":"images/icon_new/2.png",
+      "description":"uud"
+    },
+    {
       "title":"admin",
-      "picture":"icon_pop/c_definisi.png",
+      "picture":"images/icon_new/3.png",
       "description":"admin"
     },
-{
-"title":"anggaran",
-"picture":"icon_pop/c_terkait.png",
-"description":"anggaran"
-},
+    {
+      "title":"passport",
+      "picture":"images/icon_new/4.png",
+      "description":"passport"
+    },
     /*{
-      "title":"admin",
-      "picture":"images/icon_new/12.png",
-      "description":"admin"
+      "title":"hukum",
+      "picture":"images/icon_new/5.png",
+      "description":"hukum"
+    },
+    {
+      "title":"timbangan",
+      "picture":"images/icon_new/6.png",
+      "description":"timbangan"
+    },
+    {
+      "title":"kitas",
+      "picture":"images/icon_new/7.png",
+      "description":"kitas"
+    },
+    {
+      "title":"hello",
+      "picture":"images/icon_new/8.png",
+      "description":"hello"
     },*/
   ];
 
@@ -33,10 +58,13 @@ class _BodyGridState extends State<BodyGrid> {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index){
-          return SingleGrid(
-            prod_title: list_grid[index]['title'],
-            prod_pict: list_grid[index]['picture'],
-            description: list_grid[index]['description'],
+          return Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: SingleGrid(
+              prod_title: list_grid[index]['title'],
+              prod_pict: list_grid[index]['picture'],
+              description: list_grid[index]['description'],
+            ),
           );
         }
         );
@@ -58,13 +86,13 @@ class SingleGrid extends StatelessWidget {
         tag: prod_title,
         child: Material(
           child: InkWell(
-            onTap: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailScreens())),
+            onTap: (){},
             child: GridTile(
                 footer: Container(
                   color: Colors.white70,
                   child: ListTile(
                     leading: Text(prod_title, style: TextStyle(fontWeight: FontWeight.bold),),
-                    subtitle: Text(description, style: TextStyle(color: Colors.blueGrey),),
+                    title: Text(description, style: TextStyle(color: Colors.blueGrey),),
                   ),
                 ),
                 child: Image.asset(
